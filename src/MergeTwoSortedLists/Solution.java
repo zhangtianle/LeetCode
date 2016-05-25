0b29c28be702364d9732f4af39cc78a983149b62
TianleZhang <cqtianle@163.com>
2016-05-25 16:38:34 +0800

    21. Merge Two Sorted Lists

--- src/MergeTwoSortedLists/Solution.java
+++ src/MergeTwoSortedLists/Solution.java
@@ -13,35 +13,34 @@ import util.ListNodeUtil;
 public class Solution {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // TODO
+
         ListNode head = new ListNode(0);
         ListNode current = head;
-        while ( l1 != null &&l1.next != null ) {
-            ListNode tl = l1.next;
-            while ( l2 != null && l2.next != null) {
-                if (l1.val < l2.val) {
-                    current.next = new ListNode(l1.val);
-                    current = current.next;
-                    current.next = new ListNode(l2.val);
-                    current = current.next;
-                    l1 = l1.next;
-                } else {
-                    current.next = new ListNode(l2.val);
-                    current = current.next;
-                    current.next = new ListNode(l1.val);
-                    current = current.next;
-                    l2 = l2.next;
-                }
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                current.next = new ListNode(l2.val);
+                current = current.next;
+                l2 = l2.next;
+            } else {
+                current.next = new ListNode(l1.val);
+                current = current.next;
+                l1 = l1.next;
             }
+        }
+        if (l2 == null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
         }
         return head.next;
     }
 
     @Test
     public void test() {
-        ListNode l1 = ListNodeUtil.initList(2,3,4);
-        ListNode l2 = ListNodeUtil.initList(1,2,5);
-        ListNode r = mergeTwoLists(l1,l2);
+        ListNode l1 = ListNodeUtil.initList(1,9);
+        ListNode l2 = ListNodeUtil.initList(1);
+        ListNode r = mergeTwoLists(l1, l2);
+        System.out.println(r);
     }
 
 
