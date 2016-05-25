c3b7b7ab4263ec87f5067944f75223880dcb9c7d
TianleZhang <cqtianle@163.com>
2016-05-25 21:25:56 +0800

    205. Isomorphic Strings 2

--- src/IsomorphicStrings/Solution.java
+++ src/IsomorphicStrings/Solution.java
@@ -65,9 +65,23 @@ public class Solution {
         return result;
     }
 
+    public boolean isIsomorphic2(String s1, String s2) {
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
+
+        for(Integer i = 0; i < s1.length(); i++) {
+
+            if(m1.put(s1.charAt(i), i) != m2.put(s2.charAt(i), i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     @Test
     public void test() {
-        System.out.println(isIsomorphic("",""));
+        System.out.println(isIsomorphic2("ab","12"));
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -10,14 +11,10 @@ public class Main {
 
     @Test
     public void test() {
-        int[] arrayToSort = new int[] { 48, 5, 89, 80, 81, 23, 45, 16, 2 };
-        System.out.println("排序前");
-        for (int i = 0; i < arrayToSort.length; i++)
-            System.out.println(arrayToSort[i]);
-        // 调用数组的静态排序方法sort
-        Arrays.sort(arrayToSort);
-        System.out.println("排序后");
-        for (int i = 0; i < arrayToSort.length; i++)
-            System.out.println(arrayToSort[i]);
+        Map<Character,Integer> m = new HashMap<>();
+        System.out.println(m.put('a',2));
+        System.out.println(m.put('b',3));
+        System.out.println(m.put('a',1));
+        System.out.println(m.put('a',5));
     }
 }
