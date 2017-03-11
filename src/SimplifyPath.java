2b8c397ba4d204182f1aeb9ce94e56851a77d83c
TianleZhang <cqtianle@163.com>
2017-03-11 22:40:52 +0800

    Simplify Path

---
+++ src/SimplifyPath.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Given an absolute path for a file (Unix-style), simplify it.
+ * For example,
+ * path = "/home/", => "/home"
+ * path = "/a/./b/../../c/", => "/c"
+ *
+ * Created by Kyle on 2017/3/11.
+ */
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+
+        String rst = "";
+
+        String[] paths = path.split("/");
+        Stack<String> stack = new Stack();
+        for (String s : paths) {
+            if (!"".equals(s)) {
+                if("..".equals(s)) {
+                    if(!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                }else if (".".equals(s)) {
+
+                }else {
+                    stack.push(s);
+                }
+            }
+        }
+
+        for (String s : stack) {
+            rst = rst + "/" + s;
+        }
+
+        if (rst == "") {
+            rst = "/";
+        }
+
+        return rst;
+    }
+
+    @Test
+    public void test() {
+
+        System.out.println(simplifyPath("/a/../c"));
+    }
+}
