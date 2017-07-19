a35b44c745d67fd08fda65d2880c52bf2ed87347
TianleZhang <cqtianle@163.com>
2017-07-20 00:04:49 +0800

    TreeNode helper

--- src/util/TreeNode.java
+++ src/util/TreeNode.java
@@ -2,6 +2,14 @@ package util;
 
 /**
  * Created by Tianle Zhang on 2016/10/7.
+ * Given binary tree [3,9,20,null,null,15,7],
+   3
+  / \
+ 9  20
+   /  \
+  15   7
+ *
+ *
  */
 public class TreeNode {
     public int val;
@@ -10,4 +18,42 @@ public class TreeNode {
     public TreeNode(int x) {
         val = x;
     }
+
+    //    String str = "[3,9,20,null,null,15,7]";
+    public static TreeNode makeTreeNode(String str) {
+        int []arr = StrToIntArray(str);
+        TreeNode []nodes = new TreeNode[arr.length + 1];
+        for (int i = 1; i < nodes.length; i++) {
+            if (arr[i - 1] != Integer.MAX_VALUE) {
+                nodes[i] = new TreeNode(arr[i - 1]);
+            }else {
+                nodes[i] = null;
+            }
+        }
+        TreeNode node = null;
+        for (int i = 1; i < nodes.length / 2; i++) {
+            node = nodes[i];
+            if (node == null) continue;
+            node.left = nodes[2 * i];
+            node.right = nodes[2 * i + 1];
+        }
+        return nodes[1];
+    }
+
+    private static int[] StrToIntArray(String str) {
+        str = str.substring(1, str.length() - 1);
+        String []strs = str.split(",");
+        int []arr = new int[strs.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (strs[i].equals("null")) {
+                arr[i] = Integer.MAX_VALUE;
+            } else {
+                arr[i] = Integer.parseInt(strs[i]);
+            }
+        }
+
+        return arr;
+    }
+
 }
