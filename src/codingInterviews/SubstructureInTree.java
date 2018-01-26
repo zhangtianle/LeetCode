9ef6c077dd761ff2bb9a25cf29b60e3cddfbce04
TianleZhang <cqtianle@163.com>
2018-01-26 18:32:46 +0800

    树的子结构

---
+++ src/codingInterviews/SubstructureInTree.java
@@ -0,0 +1,38 @@
+package codingInterviews;
+
+import util.TreeNode;
+
+public class SubstructureInTree {
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        boolean result = false;
+        if (root1 != null && root2 != null) {
+            if (root1.val == root2.val) {
+                result = DoesTree1hasTree2(root1, root2);
+            }
+            if (!result) {
+                result = HasSubtree(root1.left, root2);
+            }
+            if (!result) {
+                result = HasSubtree(root1.right, root2);
+            }
+        }
+        return result;
+    }
+
+    private boolean DoesTree1hasTree2(TreeNode root1, TreeNode root2) {
+        if (root2 == null) {
+            return true;
+        }
+
+        if (root1 == null) {
+            return false;
+        }
+
+        if (root1.val != root2.val) {
+            return false;
+        }
+
+        return DoesTree1hasTree2(root1.left, root2.left) && DoesTree1hasTree2(root1.right, root2.right);
+    }
+
+}
