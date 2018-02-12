5124c94f676b387d356609165ee71633325d831e
ZhangTianle <cqtianle@163.com>
2018-02-12 18:48:11 +0800

    最小的k个数 （基于最大堆的实现）

--- src/codingInterviews/KLeastNumbers.java
+++ src/codingInterviews/KLeastNumbers.java
@@ -4,8 +4,47 @@ package codingInterviews;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class KLeastNumbers {
+
+    /**
+     * 基于最大堆的实现
+     * @param input
+     * @param k
+     * @return
+     */
+    public ArrayList<Integer> GetLeastNumbers_Solution2(int[] input, int k) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        if (input == null || k > input.length || k <= 0) {
+            return arrayList;
+        }
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < input.length; i++) {
+            int curr = input[i];
+            if (treeSet.size() < k) {
+                treeSet.add(curr);
+            } else {
+                int height = treeSet.last();
+                if (height > curr) {
+                    treeSet.pollLast();
+                    treeSet.add(curr);
+                }
+            }
+        }
+
+        arrayList.addAll(treeSet);
+        return arrayList;
+    }
+
+
+    /**
+     * o(n) 解法，采用快排中的partition思想
+     * @param input
+     * @param k
+     * @return
+     */
     public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
         ArrayList<Integer> arrayList = new ArrayList<>();
         QuickSort quickSort = new QuickSort();
@@ -36,6 +75,6 @@ public class KLeastNumbers {
 
     @Test
     public void test() {
-        GetLeastNumbers_Solution(new int[]{4, 5, 1, 6, 2, 7, 3, 8}, 4);
+        GetLeastNumbers_Solution2(new int[]{4, 5, 1, 6, 2, 7, 3, 8}, 4);
     }
 }
