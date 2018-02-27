4bf6a2cbf79505342c52c9af7d4b5b81c1071e75
TianleZhang <cqtianle@163.com>
2018-02-27 12:06:52 +0800

    正则表达式匹配

--- src/codingInterviews/RegularExpressionsMatching.java
+++ src/codingInterviews/RegularExpressionsMatching.java
@@ -1,11 +1,39 @@
 package codingInterviews;
 
+import org.junit.Test;
+
 public class RegularExpressionsMatching {
     public boolean match(char[] str, char[] pattern) {
         if (str == null || pattern == null) {
             return false;
         }
+        return matchCore(str, pattern, 0, 0);
+    }
+
+    private boolean matchCore(char[] str, char[] pattern, int strIndex, int patternIndex) {
+        if (strIndex == str.length && patternIndex == pattern.length) {
+            return true;
+        }
+        if (strIndex != str.length && patternIndex == pattern.length) {
+            return false;
+        }
+
+        if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
+            if ((pattern[patternIndex] == '.' && strIndex != str.length) || (strIndex != str.length && pattern[patternIndex] == str[strIndex])) {
+                return matchCore(str, pattern, strIndex + 1, patternIndex + 2) || matchCore(str, pattern, strIndex + 1, patternIndex) || matchCore(str, pattern, strIndex, patternIndex + 2);
+            } else {
+                return matchCore(str, pattern, strIndex, patternIndex + 2);
+            }
+        }
+
+        if ((pattern[patternIndex] == '.' && strIndex != str.length) || (strIndex != str.length && pattern[patternIndex] == str[strIndex])) {
+            return matchCore(str, pattern, strIndex + 1, patternIndex + 1);
+        }
         return false;
-        // TODO
+    }
+
+    @Test
+    public void test() {
+        System.out.println(match("a".toCharArray(), ".*".toCharArray()));
     }
 }
