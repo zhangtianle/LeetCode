971d2069d6ff02297f35939749faf1b318128caa
TianleZhang <cqtianle@163.com>
2018-03-29 22:47:02 +0800

    219. Contains Duplicate II

---
+++ src/leetcode/ContainsDuplicateII.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
