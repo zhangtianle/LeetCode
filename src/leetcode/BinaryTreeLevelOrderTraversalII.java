a4e112980fb911e53695d886ff348425ed89cce1
TianleZhang <cqtianle@163.com>
2018-04-05 17:47:55 +0800

    107. Binary Tree Level Order Traversal II

--- src/leetcode/BinaryTreeLevelOrderTraversalII.java
+++ src/leetcode/BinaryTreeLevelOrderTraversalII.java
@@ -1,15 +1,46 @@
 package leetcode;
 
+import org.junit.Test;
 import util.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by Kyle on 2017/7/12.
  */
 public class BinaryTreeLevelOrderTraversalII {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        //TODO
-        return null;
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                list.add(queue.poll().val);
+            }
+            lists.add(0, list);
+        }
+        return lists;
+    }
+
+    @Test
+    public void test() {
+        levelOrderBottom(TreeNode.makeTreeNode("[3,9,20,null,null,15,7]"));
     }
 }
