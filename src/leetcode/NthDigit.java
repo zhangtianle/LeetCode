319ab4295870985833139ddc232da20d7ab7d6e1
TianleZhang <cqtianle@163.com>
2018-04-21 10:57:57 +0800

    400. Nth Digit

---
+++ src/leetcode/NthDigit.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class NthDigit {
+
+    public int findNthDigit(int n) {
+        int len = 1;
+        long count = 9;
+        int start = 1;
+
+        while (n > len * count) {
+            n -= len * count;
+            len += 1;
+            count *= 10;
+            start *= 10;
+        }
+
+        start += (n - 1) / len;
+        String s = Integer.toString(start);
+        return Character.getNumericValue(s.charAt((n - 1) % len));
+    }
+
+    @Test
+    public void test() {
+        System.out.println(findNthDigit(100));
+    }
+}
