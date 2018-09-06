2e8b4ddc903983f904985b6260f4dc24b3e7a6f7
TianleZhang <cqtianle@163.com>
2018-09-06 10:54:53 +0800

    添加注释

--- src/OnlineTest/Huawei4.java
+++ src/OnlineTest/Huawei4.java
@@ -9,13 +9,18 @@ public class Huawei4 {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String str = scanner.nextLine();
+            // 正则匹配，（规则：有0个或多个负号 加上 一个或多个数字）
             String reg="(\\-*\\d+)";
             Pattern p = Pattern.compile(reg);
             Matcher m = p.matcher(str);
+            // 结果
             int sum = 0;
+            // 使用正则表达式提取字符串中所有符合规则的数字，并对每一个遍历求和
             while(m.find()) {
                 String s = m.group(1);
+                // 找到含有负号的数字
                 if (s.contains("-")) {
+                    // 判断数字前面有几个负号
                     int count = 0;
                     for (int i = 0; i < s.length(); i++) {
                         if (s.charAt(i) == '-') {
@@ -24,7 +29,10 @@ public class Huawei4 {
                             break;
                         }
                     }
+                    // 将数字转为int类型
                     int t = Integer.parseInt(s.substring(count, s.length()));
+
+                    // 如果含有偶数个负号，则为正，否则为负
                     if (count % 2 == 0) {
                         sum += t;
                     } else {
@@ -35,7 +43,6 @@ public class Huawei4 {
                 }
             }
             System.out.println(sum);
-
         }
     }
 
